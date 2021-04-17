502ff907ce13e51b2014ea3e513072156875e349
carlos acitores deval <carlos.acitores@colba.es>
2021-04-17 11:46:42 +0200

    rename

--- backend/src/images/core/service/ImageRepository.ts
+++
@@ -1,7 +0,0 @@
-import { Image } from "../../../shared/images/core/entities/Image";
-
-export interface ImageRepository {
-    Upload(image: any): Promise<void>;
-    Read(image: any): Promise<Image>;
-    Delete(image: any): Promise<void>;
-}
---
+++ backend/src/images/core/services/ImageStorage.ts
@@ -0,0 +1,9 @@
+import { Image } from "../../../shared/images/core/entities/Image";
+import { ImageBytes } from "../../../shared/images/core/valueObjects/ImageBytes";
+import { ImageName } from "../valueObjects/ImageName";
+
+export interface ImageStorage {
+    Upload(imageName: ImageName, imageBytes: ImageBytes): Promise<void>;
+    Read(imageName: ImageName): Promise<URL>;
+    Delete(imageName: ImageName): Promise<void>;
+}

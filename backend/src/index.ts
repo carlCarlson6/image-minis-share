8ca04cdcb1444d17ece6ad165d3ac26e10e9e834
carlos acitores deval <carlos.acitores@colba.es>
2021-04-17 19:41:33 +0200

    misc

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,11 @@
 import dotenv from 'dotenv';
 import { Server } from './shared/common/infrastructure/api/Server';
+import { registerDependencies } from './shared/common/infrastructure/dependencyInjection/RegisterDependencies';
 
 const bootstrap = async () => {
     dotenv.config({path:'dev.env'});
+
+    registerDependencies();
 
     const server: Server = new Server();
     server.Start(); 
